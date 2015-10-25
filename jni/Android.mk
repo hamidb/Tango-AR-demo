587463e78d0e776aad06b6915fb5420e224b4dff
Hamid Bazargani <hamidbazargani@gmail.com>
2015-10-25 11:43:42 -0400

    Uses OpenCV to convert from YUV to RGB
    release-yamabe
    This uses OpenCV for faster conversion.

---
+++ jni/Android.mk
@@ -0,0 +1,47 @@
+#
+# Copyright 2014 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+LOCAL_PATH := $(call my-dir)
+TANGO_ROOT:= /hamidb/software/applications/tango-examples-c
+
+include $(CLEAR_VARS)
+INSTALL_CUDA_LIBRARIES:=on
+OPENCV_INSTALL_MODULES:=on
+CUDA_TOOLKIT_DIR=$(CUDA_TOOLKIT_ROOT)
+NDK_TOOLCHAIN_VERSION = 4.8
+include ../../OpenCV-2.4.8.2-Tegra-sdk/sdk/native/jni/OpenCV-tegra3.mk
+
+LOCAL_MODULE    := myTangoProject
+LOCAL_SHARED_LIBRARIES += tango_client_api
+LOCAL_CFLAGS    := -Werror -std=c++11
+LOCAL_SRC_FILES := tango_native.cc \
+                   tango_handler.cc \
+                   yuv_drawable.cc \
+                   $(TANGO_ROOT)/tango-gl/camera.cpp \
+                   $(TANGO_ROOT)/tango-gl/line.cpp \
+                   $(TANGO_ROOT)/tango-gl/util.cpp \
+                   $(TANGO_ROOT)/tango-gl/transform.cpp \
+                   $(TANGO_ROOT)/tango-gl/drawable_object.cpp \
+                   $(TANGO_ROOT)/tango-gl/shaders.cpp \
+                   $(TANGO_ROOT)/tango-gl/video_overlay.cpp
+
+LOCAL_C_INCLUDES += $(TANGO_ROOT)/tango-gl/include \
+                    $(TANGO_ROOT)/third-party/glm \
+                    tango-video-handler
+LOCAL_LDLIBS    := -llog -lGLESv2 -L$(SYSROOT)/usr/lib
+include $(BUILD_SHARED_LIBRARY)
+
+$(call import-add-path, $(TANGO_ROOT))
+$(call import-module,tango_client_api)
---
+++ src/com/project/tango/TangoARActivity.java
@@ -0,0 +1,103 @@
+/*
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.project.tango;
+
+import com.project.tango.R;
+
+import android.app.Activity;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ToggleButton;
+import android.util.Log;
+import android.util.Log;
+// Main activity
+public class TangoARActivity extends Activity
+    implements View.OnClickListener {
+  public enum TextureMethod {
+    YUV,
+    TEXTURE_ID
+  }
+
+  private GLSurfaceView glView;
+  private ToggleButton mYUVRenderSwitcher;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    setContentView(R.layout.activity_video_overlay);
+    glView = (GLSurfaceView) findViewById(R.id.surfaceview);
+    glView.setRenderer(new Renderer());
+
+    mYUVRenderSwitcher = (ToggleButton) findViewById(R.id.yuv_switcher);
+    mYUVRenderSwitcher.setOnClickListener(this);
+
+    // Initialize Tango Service, this function starts the communication
+    // between the application and Tango Service.
+    // The activity object is used for checking if the API version is outdated.
+    TangoJNINative.initialize(this);
+  }
+  
+  @Override
+  protected void onResume() {
+    super.onResume();
+    glView.onResume();
+    // Setup the configuration for the TangoService.
+    TangoJNINative.setupConfig();
+
+    // Connect to Tango Service.
+    // This function will start the Tango Service pipeline, in this case,
+    // it will start Motion Tracking.
+    TangoJNINative.connect();
+
+    EnableYUVTexture(mYUVRenderSwitcher.isChecked());
+  }
+
+  @Override
+  protected void onPause() {
+    super.onPause();
+    glView.onPause();
+    // Disconnect from Tango Service, release all the resources that the app is
+    // holding from Tango Service.
+    TangoJNINative.disconnect();
+    TangoJNINative.freeGLContent();
+  }
+
+  @Override
+  public void onClick(View v) {
+    switch (v.getId()) {
+    case R.id.yuv_switcher:
+      EnableYUVTexture(mYUVRenderSwitcher.isChecked());
+      break;
+    }
+  }
+
+  @Override
+  protected void onDestroy() {	
+    super.onDestroy();
+  }
+
+  private void EnableYUVTexture(boolean isEnabled) {
+    if (isEnabled) {
+        // Turn on YUV
+        TangoJNINative.setYUVMethod();
+      } else {
+        TangoJNINative.setTextureMethod();
+      }
+  }
+}
